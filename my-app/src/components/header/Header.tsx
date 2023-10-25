24261661071ebd9cb64fc12254b21beab6a7abb3
den <mac@macbook-pro-mac.localdomain>
2023-10-25 16:37:43 +0200

    navbar added

--- my-app/src/components/header/Header.tsx
+++ my-app/src/components/header/Header.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+import classes from "./Header.module.css";
+
 export default function Header() {
   return (
-    <header>
-      Header
-      <></>
+    <header className={classes.header}>
+      <nav className={classes.nav}>
+        <Link href="/">Главная</Link>
+        <Link href="/socials">Переходы</Link>
+        <Link href="/news">Инфо-раздел</Link>
+        <Link href="/">О нас</Link>
+      </nav>
     </header>
   );
 }
