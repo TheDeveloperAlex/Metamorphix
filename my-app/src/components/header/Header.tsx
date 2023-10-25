866f9648828c25e9f521b9f36b3635a3dcd8543b
den <mac@macbook-pro-mac.localdomain>
2023-10-25 17:57:07 +0200

    header

--- my-app/src/components/header/Header.tsx
+++ my-app/src/components/header/Header.tsx
@@ -1,14 +1,23 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import classes from "./Header.module.css";
+import { links } from "./Header.constants";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className={classes.header}>
       <nav className={classes.nav}>
-        <Link href="/">Главная</Link>
-        <Link href="/socials">Переходы</Link>
-        <Link href="/news">Инфо-раздел</Link>
-        <Link href="/">О нас</Link>
+        {
+          links.map(({ href, label }) => {
+            return (
+              <Link key={href} href={href} className={pathname === `${href}` ? classes.active : ""}>{label}</Link>
+            );
+          })
+        }
       </nav>
     </header>
   );
