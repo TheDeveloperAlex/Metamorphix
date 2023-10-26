320ae283d67e4dcc5a5636088421b261ca7de563
TheDeveloperAlex <scorpion422@ukr.net>
2023-10-26 14:12:39 +0200

    fix buttons

--- my-app/src/components/PostCard.tsx
+++ my-app/src/components/PostCard.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import { Skeleton } from "./ui/skeleton";
-import { Button } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import Link from "next/link";
 import { Post } from "../../posts";
+import { cn } from "@/lib/utils";
 
 export default function PostCard({
   date,
@@ -17,16 +18,20 @@ export default function PostCard({
         <Skeleton className="w-full h-full bg-black bg-opacity-50" />
         <Image src={image} alt="Preview" fill />
       </div>
-      <div className="flex flex-col gap-6 px-5 pt-[26px] pb-12 relative text-[12px] leading-normal grow">
+      <div className="flex flex-col gap-6 px-5 pt-[26px] pb-12 relative text-[12px] !leading-normal grow">
         <span className="absolute right-3 top-3">
           {date.toLocaleDateString()}
         </span>
         <h2 className="text-[16px] text-white">{title}</h2>
         <p className="overflow-clip shrink max-h-[120px]">{description}</p>
-        <Link href={`news/${slug}`} className="mt-auto self-center ">
-          <Button variant="red-hover" className="px-5">
-            Подробнее..
-          </Button>
+        <Link
+          href={`news/${slug}`}
+          className={buttonVariants({
+            variant: "red-hover",
+            className: "mt-auto self-center px-5 !text-[12px]",
+          })}
+        >
+          Подробнее...
         </Link>
       </div>
     </div>
