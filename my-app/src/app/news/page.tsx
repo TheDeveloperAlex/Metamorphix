7d8b89dde8ecefdfc7e0be03b5a9f31ac55e033e
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2023-11-27 15:55:08 +0000

    fix title

--- my-app/src/app/news/page.tsx
+++ my-app/src/app/news/page.tsx
@@ -5,14 +5,14 @@ import Footer from "@/components/footer/Footer";
 export default function News() {
   return (
     <div
-      className="flex flex-col items-center gap-[60px]"
+      className="flex flex-col items-center gap-[50px] pt-[40px] lg:pt-[80px]"
       style={{ fontFamily: "TacticSans-Bld, sans-serif" }}
     >
-      <h1 className="text-[40px] text-center leading-[40px] text-white relative">
-        <span className="absolute w-[200px] h-[66px] bg-red-header left-0 -z-10 blur-[100px]" />
-        НОВОСТИ
+      <h1 className="min-[560px]:text-[40px] min-[420px]:text-[32px] text-[24px] text-center leading-[40px] text-white relative uppercase">
+        <span className="absolute w-[80%] h-[66px] bg-red-header left-1/2 -translate-x-1/2 -z-10 blur-[100px]" />
+        Про
         <br />
-        <span className="text-[48px] text-red-header">СЕРВЕРА</span>
+        <span className="min-[560px]:text-[48px] min-[420px]:text-[40px] text-[30px] text-red-header">метаморфикс</span>
       </h1>
       <div
         className="grid grid-cols-2 gap-[54px] mt-5"
